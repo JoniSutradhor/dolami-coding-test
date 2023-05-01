74059d1683a393026ba2189a09f72e7f1fc1e73c
J.K.Sutradhor <jksutradhor@gmail.com>
2023-05-01 20:18:00 +0600

    Product init

--- dolami-coding-test/src/components/contents/Contents.js
+++ dolami-coding-test/src/components/contents/Contents.js
@@ -8,7 +8,64 @@ const Contents = ()=> {
                     <LeftPanel />
                 </div>
                 <div className="w-4/5 border-2 border-amber-800">
-                    Body
+                    <div className="grid grid-cols-1 lg:grid-cols-4 justify-items-center">
+                        <div className="py-10">
+                            <div className="rounded overflow-hidden max-w-sm">
+                                <img src="logo192.png" alt="" className="w-full "/>
+                                <div className="px-6 py-4">
+                                    <div className="font-bold text-xl mb-2"></div>
+                                    <p className="text-gray-500">Kaccu baccu details</p>
+                                </div>
+                                <div className="grid grid-flow-col gap-4 pb-2 px-6">
+                                    <span className="bg-gray-200 rounded-full px-3 py-1 font-base mb-2">kattu</span>
+                                    <span className="bg-gray-200 rounded-full px-3 py-1 font-base mb-2">battu</span>
+                                    <span className="bg-gray-200 rounded-full px-3 py-1 font-base mb-2">kuttu</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="py-10">
+                            <div className="rounded overflow-hidden max-w-sm">
+                                <img src="logo192.png" alt="" className="w-full "/>
+                                <div className="px-6 py-4">
+                                    <div className="font-bold text-xl mb-2"></div>
+                                    <p className="text-gray-500">Kaccu baccu details</p>
+                                </div>
+                                <div className="grid grid-flow-col gap-4 pb-2 px-6">
+                                    <span className="bg-gray-200 rounded-full px-3 py-1 font-base mb-2">kattu</span>
+                                    <span className="bg-gray-200 rounded-full px-3 py-1 font-base mb-2">battu</span>
+                                    <span className="bg-gray-200 rounded-full px-3 py-1 font-base mb-2">kuttu</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="py-10">
+                            <div className="rounded overflow-hidden max-w-sm">
+                                <img src="logo192.png" alt="" className="w-full "/>
+                                <div className="px-6 py-4">
+                                    <div className="font-bold text-xl mb-2"></div>
+                                    <p className="text-gray-500">Kaccu baccu details</p>
+                                </div>
+                                <div className="grid grid-flow-col gap-4 pb-2 px-6">
+                                    <span className="bg-gray-200 rounded-full px-3 py-1 font-base mb-2">kattu</span>
+                                    <span className="bg-gray-200 rounded-full px-3 py-1 font-base mb-2">battu</span>
+                                    <span className="bg-gray-200 rounded-full px-3 py-1 font-base mb-2">kuttu</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="py-10">
+                            <div className="rounded overflow-hidden max-w-sm">
+                                <img src="logo192.png" alt="" className="w-full "/>
+                                <div className="px-6 py-4">
+                                    <div className="font-bold text-xl mb-2"></div>
+                                    <p className="text-gray-500">Kaccu baccu details</p>
+                                </div>
+                                <div className="grid grid-flow-col gap-4 pb-2 px-6">
+                                    <span className="bg-gray-200 rounded-full px-3 py-1 font-base mb-2">kattu</span>
+                                    <span className="bg-gray-200 rounded-full px-3 py-1 font-base mb-2">battu</span>
+                                    <span className="bg-gray-200 rounded-full px-3 py-1 font-base mb-2">kuttu</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </>
